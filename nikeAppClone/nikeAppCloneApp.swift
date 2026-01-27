74f258807853018150c60868c16936f2964477cc
kimmulgae <kimmulgae@icloud.com>
2026-01-27 09:48:26 +0900

    first commit

---
+++ nikeAppClone/nikeAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  nikeAppCloneApp.swift
+//  nikeAppClone
+//
+//  Created by Sunghyun Kim on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct nikeAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nikeAppCloneTests/nikeAppCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  nikeAppCloneTests.swift
+//  nikeAppCloneTests
+//
+//  Created by Sunghyun Kim on 1/22/26.
+//
+
+import Testing
+@testable import nikeAppClone
+
+struct nikeAppCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
