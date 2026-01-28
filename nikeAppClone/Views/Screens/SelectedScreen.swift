118e41aa620be86f66c2c8e707d6124b308b8eeb
kimmulgae <kimmulgae@icloud.com>
2026-01-28 17:21:10 +0900

    Add tabView

--- nikeAppClone/Views/Screens/SelectedScreen.swift
+++ nikeAppClone/Views/Screens/SelectedScreen.swift
@@ -10,18 +10,20 @@ import SwiftUI
 struct SelectedScreen: View {
     @Binding var selectedTab: Tab
 
+    @State private var homePath = NavigationPath()
+    @State private var shopPath = NavigationPath()
+    @State private var favPath = NavigationPath()
+    @State private var bagPath = NavigationPath()
+    @State private var profilePath = NavigationPath()
+
     var body: some View {
-        switch selectedTab {
-        case .home:
-            NavigationStack { HomeScreen() }
-        case .shop:
-            NavigationStack { ShopScreen() }
-        case .favourites:
-            NavigationStack { FavouritesScreen() }
-        case .bag:
-            NavigationStack { BagScreen() }
-        case .profile:
-            NavigationStack { ProfileScreen() }
+        TabView(selection: $selectedTab) {
+            NavigationStack(path: $homePath) { HomeScreen() }.tag(Tab.home)
+            NavigationStack(path: $shopPath) { ShopScreen() }.tag(Tab.shop)
+            NavigationStack(path: $favPath) { FavouritesScreen() }.tag(Tab.favourites)
+            NavigationStack(path: $bagPath) { BagScreen() }.tag(Tab.bag)
+            NavigationStack(path: $profilePath) { ProfileScreen() }.tag(Tab.profile)
         }
+        .toolbar(.hidden, for: .tabBar)
     }
 }
